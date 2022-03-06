fa712d47efaa92d71b4acb60c656bc8d8b443528
action-ukon <actionukon0614mhf@gmail.com>
2022-03-06 11:31:26 +0900

    Initial Commit

---
+++ map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  map
+//
+//  Created by 坪田和樹 on 2022/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by 坪田和樹 on 2022/03/06.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
